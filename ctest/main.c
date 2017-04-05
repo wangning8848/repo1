7dd4f59cb4f25c77433a27ae3f8008a09f6a8226
Ning Wang <wangning8848@gmail.com>
2017-04-05 21:14:48 +0200

    takeTwoStones

--- ctest/main.c
+++ ctest/main.c
@@ -3,6 +3,9 @@
 
 int main()
 {
+    char a[] = "99";
     printf("Hello world!\n");
+    int i = atoi(a);
+    printf("i is %d",i);
     return 0;
 }
---
+++ takeTwoStones/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    scanf("%d",&i);
+    if(i%2==1)
+        printf("Alice");
+    else
+        printf("Bob");
+    return 0;
+}
