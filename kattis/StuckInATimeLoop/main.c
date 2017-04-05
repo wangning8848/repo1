8eb3fcfad4f927867e4d73bc69954c28f622b8f3
Ning Wang <wangning8848@gmail.com>
2017-04-05 21:23:42 +0200

    StuckInATimeLoop

---
+++ kattis/StuckInATimeLoop/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    scanf("%d",&i);
+    int j;
+    for (j=1;j<=i;j++)
+    {
+       printf("%d Abracadabra\n",j);
+    }
+    return 0;
+}
