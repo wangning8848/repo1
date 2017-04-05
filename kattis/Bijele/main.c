b9a162c88e8ac857cd2c4fcfabe041f3a6763884
Ning Wang <wangning8848@gmail.com>
2017-04-05 21:53:43 +0200

    kattis

---
+++ kattis/Bijele/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int k,q,r,b,kn,p;
+    scanf("%d %d %d %d %d %d",&k,&q,&r,&b,&kn,&p);
+    printf("%d %d %d %d %d %d",1-k,1-q,2-r,2-b,2-kn,8-p);
+
+    return 0;
+}
